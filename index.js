0d372a8b22f8c9d0fe10eaae5c9fe474ec227ede
calixto14 <calixto@live.com>
2023-07-13 20:25:17 -0300

    criando o primeiro serviço

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const amqp = require("amqplib");
+var channel, connection;  
+const app = express();
+const PORT = process.env.PORT || 4001;
+app.use(express.json());
+app.get("/send-msg", async(req, res) => {
+    await sendData({log : "ok"})
+    res.send("Hello world")
+});
+app.listen(PORT, async() => {
+    await connectQueue();
+    console.log("Server running at port " + PORT)
+});
+
+
+async function connectQueue() {   
+    try {
+        connection = await amqp.connect("amqp://localhost:5672");
+        channel    = await connection.createChannel()
+        
+        await channel.assertQueue("test-queue")
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function sendData (data) {
+    // send data to queue
+    await channel.sendToQueue("test-queue", Buffer.from(JSON.stringify(data)));
+        
+    // close the channel and connection
+    await channel.close();
+    await connection.close(); 
+}
